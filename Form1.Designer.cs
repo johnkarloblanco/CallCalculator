cd784079e13afa9b009532fd7907b04a6159a3e0
John Blanco <51978928+johnkarloblanco@users.noreply.github.com>
2020-06-02 18:31:10 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace Assign5
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lengthBox = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.rateBox = new System.Windows.Forms.TextBox();
+            this.minBox = new System.Windows.Forms.TextBox();
+            this.totalBox = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(130, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(94, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Select calling area";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(9, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(72, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Length of Call";
+            // 
+            // lengthBox
+            // 
+            this.lengthBox.Location = new System.Drawing.Point(12, 32);
+            this.lengthBox.Name = "lengthBox";
+            this.lengthBox.Size = new System.Drawing.Size(100, 20);
+            this.lengthBox.TabIndex = 2;
+            this.lengthBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            this.lengthBox.TextChanged += new System.EventHandler(this.lengthBox_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(154, 31);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(70, 21);
+            this.comboBox1.TabIndex = 4;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(18, 86);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(51, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Rate/min";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(130, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(87, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Minimum minutes";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(296, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(80, 13);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Total Charge";
+            // 
+            // rateBox
+            // 
+            this.rateBox.Enabled = false;
+            this.rateBox.Location = new System.Drawing.Point(21, 103);
+            this.rateBox.Name = "rateBox";
+            this.rateBox.Size = new System.Drawing.Size(75, 20);
+            this.rateBox.TabIndex = 8;
+            this.rateBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // minBox
+            // 
+            this.minBox.Enabled = false;
+            this.minBox.Location = new System.Drawing.Point(142, 103);
+            this.minBox.Name = "minBox";
+            this.minBox.Size = new System.Drawing.Size(75, 20);
+            this.minBox.TabIndex = 9;
+            this.minBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // totalBox
+            // 
+            this.totalBox.Enabled = false;
+            this.totalBox.Location = new System.Drawing.Point(267, 102);
+            this.totalBox.Name = "totalBox";
+            this.totalBox.Size = new System.Drawing.Size(107, 20);
+            this.totalBox.TabIndex = 10;
+            this.totalBox.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(424, 294);
+            this.Controls.Add(this.totalBox);
+            this.Controls.Add(this.minBox);
+            this.Controls.Add(this.rateBox);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.lengthBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox lengthBox;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox rateBox;
+        private System.Windows.Forms.TextBox minBox;
+        private System.Windows.Forms.TextBox totalBox;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Assign5
+{
+    public partial class Form1 : Form
+    {
+        private string[] areaCode = {"236", "250", "778", "507", "780", "805", "403",
+                                     "587", "825", "306", "639", "204", "432", "807",
+                                     "249", "705", "226", "519", "548", "709"};
+        private double[] rate = { .05, .05, .05, .03, .03, .03, .04, .06, .06, .06,
+                                  .08, .08, .08, .09, .09, .09, .10, .10, .12, .14};
+
+        private int[] minTime = { 3, 3, 3, 0, 0, 0, 5, 5, 5, 2,
+                                  2, 2, 4, 4, 4, 6, 6, 6, 8, 9};
+        int totalMinutes;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Shutdown the painting of the comboBox as items are added.
+            comboBox1.BeginUpdate();
+            lengthBox.Enabled = false;
+
+            //load comboBox with the area codes
+            //use the method comboBox1.Items.Add("403");
+            int s1 = areaCode.Length - 1;
+
+            for (int i = 0; i <= s1; i++)
+            {
+                comboBox1.Items.Add(areaCode[i]);
+            }
+
+            // Allow the comboBox to repaint and display the new items.
+            comboBox1.EndUpdate();
+        }
+
+
+        // comboBox was changed - calculate the charges for the phone call
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            lengthBox.Enabled = true; //the lengthbox will only be enabled once the user selects an area code
+            valueProcessor();
+        }
+        
+        private void lengthBox_TextChanged(object sender, EventArgs e)
+        {
+            valueProcessor();
+        }
+        
+        private void valueProcessor ()
+        {
+            int selectedIndex = comboBox1.SelectedIndex; //this is the array's address
+
+            if (lengthBox.Text == "")
+            {
+                int tempVal = 0;
+                lengthBox.Text = tempVal.ToString();
+            }
+
+            minBox.Text = minTime[selectedIndex].ToString();
+            rateBox.Text = rate[selectedIndex].ToString();
+
+            //if the length is less than the minimum, the cost would be length * rate
+            //if the length is more than the minimum, the cost would be (length - minimum) * rate
+
+            int lengthBoxVal = Int32.Parse(lengthBox.Text);
+            int minTimeVal = minTime[selectedIndex];
+            double rateVal = rate[selectedIndex];
+            double totalVal = 0;
+
+            if (lengthBoxVal > minTimeVal)//length is greater than minimum
+            {
+                totalMinutes = lengthBoxVal - minTimeVal;
+                totalVal = rateVal + (totalMinutes * rateVal);
+                totalBox.Text = "$" + totalVal.ToString("0.00");
+            }
+            else //length of call is less than minimum
+            {
+                if (lengthBoxVal == 0)
+                {
+                    totalBox.Text = "$ 0.00";
+                } else
+                {
+                    totalMinutes = lengthBoxVal;
+                    totalVal = rateVal;
+                    totalBox.Text = "$ " + totalVal.ToString("0.00");
+                }
+
+            }
+        }
+    }
+}
